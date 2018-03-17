beb10e23dc391410f3e8bc0478b8846e889dcd33
Mohamed Aghzal <34104192+MohamedAghzal@users.noreply.github.com>
2018-03-17 23:36:52 +0000

    Added timer

--- Multiple_Feeds.py
+++ Multiple_Feeds.py
@@ -1,7 +1,11 @@
 #Import the necessary packages
 from tkinter import * #Make sure you're using Python 3!!!
+
+import datetime
 from cv2 import *
 from sys import *
+from time import *
+import daytime
 
 cam1 = VideoCapture(0) #Capture the video from camera on port 0
 cam2 = VideoCapture(1) #Capture the video from camera on port 1
@@ -12,8 +16,8 @@ height = maxsize
 #Set the dimensions for the main camera
 cam1.set(CAP_PROP_FRAME_WIDTH, width)
 cam1.set(CAP_PROP_FRAME_HEIGHT, height)
-
 #Set the dimensions for the secondary camera
+
 cam2.set(CAP_PROP_FRAME_WIDTH, 200)
 cam2.set(CAP_PROP_FRAME_HEIGHT, 180)
 
@@ -22,24 +26,39 @@ cams = [cam1, cam2]
 
 root = Tk()
 label = Label(root)
+startTime = datetime.datetime.now()
 
 label.pack(side = TOP, fill = BOTH, expand = YES)
 
 index =  0  #This variable allows us to switch between the list indexes:
             # if index is 0: the main camera will be that on the first port
             # if index is 1: the main camera will be that on the second port
-
+secCount = 0
+min = 0
 while (True):
     var, frame = cams[index].read() #Accessing the camera at the corresponding index (Main Camera)
     var, frame2 = cams[1 - index].read() #Accessing the other camera (Secondary camera)
-
     #Making the feed taken by the secondary camera appear on top of the main one
 
     frame[10:10 + frame2.shape[0], 10:10 + frame2.shape[1]] = frame2
+    currSec = '0' + str(secCount) if secCount < 10 else str(secCount)
+    currTime = '0' + str(min) + ':' + str(secCount) if min < 10 else str(min) + ':' + currSec
+    putText(img = frame, text = currTime, org = (1000, 90), fontFace= FONT_HERSHEY_DUPLEX, fontScale = 3,
+                    color = (160, 45, 45), thickness = 4, lineType= CV_8S)
+    timeElapsed = (datetime.datetime.now() - startTime).total_seconds()
+    if (timeElapsed >= 1):
+        secCount += 1
+        timeElapsed = 0
+        startTime = datetime.datetime.now()
+    if (secCount == 60):
+        secCount = 1
+        min += 1
+
     imshow('Frame', frame)
     key = waitKey(1) & 0xFF
     if(key == ord('S') or key == ord('s') or key == 13): #Press S or ENTER to switch the feed
-        index = 1 - index #Changing the index
+
+        index = 1 - index #Changing the index`
 
         # Switch the sizes of the feeds
 
@@ -56,6 +75,7 @@ while (True):
 
             cam2.set(CAP_PROP_FRAME_WIDTH, width)
             cam2.set(CAP_PROP_FRAME_HEIGHT, height)
+
     if (key == 27 or key == ord('Q') or key == ord('q')): #Press ESC or Q to quit
             break
 
